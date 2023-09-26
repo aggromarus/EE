2e57a8716e02d32612408a129ee1d1939941d98f
Aggromarus <aggromarus@yandex.ru>
2023-09-27 02:08:13 +0300

    разметка с шахтой и лифтом

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,8 @@
 <template>
   <main>
     <div class="main__section">
-      здание
-      <div class="elevator wrapper">
-        лифт
+      <div class="wrapper">
+        <div class="elevator">лифт</div>
       </div>
       <div class="levels">
         <div class="level">1
@@ -31,25 +30,42 @@
 
 <style>
 .main__section {
+  background-color: cornflowerblue;
   width: 350px;
   height: 500px;
   border: 1px solid red;
-}
-
-.elevator {
-  width: 40px;
-  height: 60px;
-  border: 3px lightcoral dashed;
+  position: absolute;
 }
 
 .levels {
+  display: flex;
+  justify-content: stretch;
+  flex-direction: column-reverse;
   background-color: grey;
   height: 100%;
   width: 100%;
+}
+.level{
+  position: relative;
+  height: 100%;
 }
 .btn{
   margin: 0;
   padding: 5px;
   border-radius: 30px;
+}
+.wrapper {
+  position: absolute;
+  background-color: green;
+  width: 50px;
+  right: 1px;
+  height: 100%;
+}
+.elevator {
+  width: 40px;
+  height: 60px;
+  border: 3px lightcoral dashed;
+  background-color: antiquewhite;
+  position: absolute;
 }
 </style>
